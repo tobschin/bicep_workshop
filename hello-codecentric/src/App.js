c21f9d16a0fc1421ad5b6a2f66e5554c4ec6f90e
tobiasschinagl <tobias.schinagl@codecentric.de>
2023-03-24 14:11:11 +0100

    add webapp

---
+++ hello-codecentric/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://www.nik-nbg.de/fileadmin/_processed_/6/1/csm_16-9_codecentri-Logo_8683eda41e.png' className="App-logo" alt="logo" />
+        <p>
+          Congrats you just deployed me.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
